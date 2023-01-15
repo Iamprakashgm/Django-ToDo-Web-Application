29c3bcd358ab331aca3920d305d162e711d60c02
Prakash Gharti Magar <prakashgm668.work@gmail.com>
2023-01-16 00:30:30 +0545

    Changed

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = "django-insecure-*w-(jz@w%4ai^7^0a(a_0-)0^#48g6!6s901&9=mcgg@n9t4k2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -102,9 +102,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join('static')
+    os.path.join(BASE_DIR, 'static')
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
